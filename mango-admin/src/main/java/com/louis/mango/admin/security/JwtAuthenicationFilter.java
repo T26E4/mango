bfcf303bfe1488437761c7e3010cda5f287680f4
T26E4 <1391438034@qq.com>
2021-05-13 18:10:33 +0800

    update

--- mango-admin/src/main/java/com/louis/mango/admin/security/JwtAuthenicationFilter.java
+++ mango-admin/src/main/java/com/louis/mango/admin/security/JwtAuthenicationFilter.java
@@ -1,5 +1,12 @@
 package com.louis.mango.admin.security;
 
-public class JwtAuthenicationFilter {
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JwtAuthenicationFilter extends BasicAuthenticationFilter{
+
+	public JwtAuthenicationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
 
 }
