7531a562643289349250d0b7be53cbbe79d82d8d
T26E4 <1391438034@qq.com>
2021-05-13 13:24:01 +0800

    update

--- mango-admin/src/main/java/com/louis/mango/admin/util/SecurityUtils.java
+++ mango-admin/src/main/java/com/louis/mango/admin/util/SecurityUtils.java
@@ -3,7 +3,6 @@
 //import javax.servlet.http.HttpServletRequest;
 //
 //import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.core.Authentication;
 //import org.springframework.security.core.context.SecurityContextHolder;
 //import org.springframework.security.core.userdetails.UserDetails;
 //import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
