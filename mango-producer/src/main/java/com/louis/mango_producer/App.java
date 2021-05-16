6fa50d1fee1aaf1895e98dc40c1319f8768e1e58
liutong <1391438034@qq.com>
2021-05-16 20:47:36 +0800

    添加mango-producer项目

---
+++ mango-producer/src/main/java/com/louis/mango_producer/App.java
@@ -0,0 +1,13 @@
+package com.louis.mango_producer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
