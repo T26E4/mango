70ec5104dd0c05497b8528dde8560ec268bbf663
liutong <1391438034@qq.com>
2021-05-16 20:23:27 +0800

    集成consul

--- mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
+++ mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
@@ -2,8 +2,10 @@ package com.louis.mango.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //默认扫描启动类包及其子包，scanBasePackages不配置也可以
 @SpringBootApplication(scanBasePackages = {"com.louis.mango"})
+@EnableDiscoveryClient
 public class MangoAdminApplication {
 
 	public static void main(String[] args) {
