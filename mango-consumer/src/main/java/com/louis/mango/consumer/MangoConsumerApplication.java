a6f01fda0f88c57de83aea2c60ed7b4c1f6c849e
liutong <1391438034@qq.com>
2021-05-23 12:07:21 +0800

    添加hystrix监控  hystrix  dashboard

--- mango-consumer/src/main/java/com/louis/mango/consumer/MangoConsumerApplication.java
+++ mango-consumer/src/main/java/com/louis/mango/consumer/MangoConsumerApplication.java
@@ -2,11 +2,14 @@ package com.louis.mango.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -21,4 +24,20 @@ public class MangoConsumerApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	
+	//添加的这部分是服务监控，跟feign中的hystrix服务容错本身没有关系
+	@Bean
+	public ServletRegistrationBean<HystrixMetricsStreamServlet> getServletRegistrationBean(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		
+		ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		
+		return registrationBean;
+		
+	}
+	
 }
